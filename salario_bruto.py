020da7dec8ae2b3f2cf4a061287ab10308bd6eb1
leoRDZ17 <26150653@aguascalientes.tecnm.mx>
2026-07-20 19:57:57 -0600

    Programa basico en python

--- salario_bruto.py
+++ salario_bruto.py
@@ -1,6 +1,6 @@
 salario = float(input("Salario bruto: "))
 porcentaje = float(input("% Impuestos: "))
-deducciones = float(input("Deducciones"))
+deducciones = float(input("Deducciones: "))
 
 impuesto = salario * (porcentaje / 100)
 salario_net = salario-impuesto-deducciones
